49b49385c3a3e4bb8dfce004b9cdb279399de652
Bablu Roy <petrospine@gmail.com>
2021-04-24 14:59:22 +0530

    bugs fixed

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDJReJX68iXsm3ezBleCphQssMX_JHeiA0",
-    authDomain: "beta-testing-e20d1.firebaseapp.com",
-    databaseURL: "https://beta-testing-e20d1.firebaseio.com",
-    projectId: "beta-testing-e20d1",
-    storageBucket: "beta-testing-e20d1.appspot.com",
-    messagingSenderId: "241161178252",
-    appId: "1:241161178252:web:b0b7ed565576ccd6727fa9"
+    //paste your config here
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
